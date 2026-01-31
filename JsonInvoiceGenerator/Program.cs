6b4b37231087bdac4708029abd37839c60cd6076
Ashutosh Joshi <ashutoshjosheee@gmail.com>
2026-01-31 15:07:14 +0530

    -Added dotnet console app

---
+++ JsonInvoiceGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JsonInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
